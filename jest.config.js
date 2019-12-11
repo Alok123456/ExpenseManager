d1237f308fc0a79940ffb3a5f162d331120fcc65
Alok Rai <alok.rai@magicsw.com>
2019-12-11 18:46:53 +0530

    pushed changes for unit testing configuration

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  preset: 'ts-jest/presets/js-with-ts',
-  transformIgnorePatterns: ["/node_modules/(?!vue-awesome)"],
-  // preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel"
+  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel"
 };
